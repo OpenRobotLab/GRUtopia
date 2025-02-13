f5b9dfe197992fc9a6cdeec82f3466b40a2e3528
yezichao <danielwsam@gmail.com>
2025-02-13 13:13:59 +0800

    [fix] Restore mocap_controlled_camera

--- grutopia_extension/sensors/__init__.py
+++ grutopia_extension/sensors/__init__.py
@@ -1,3 +1,1 @@
-from grutopia_extension.sensors import camera, rep_camera
-
-# from grutopia_extension.sensors import camera, mocap_controlled_camera, rep_camera
+from grutopia_extension.sensors import camera, mocap_controlled_camera, rep_camera
